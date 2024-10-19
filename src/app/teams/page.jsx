35b9668e066bd900d0ef894f57d59f34492f2691
om salunke <omsaunke343@gmail.com>
2024-10-19 14:42:14 +0530

    change in styled

--- src/app/teams/page.jsx
+++ src/app/teams/page.jsx
@@ -5,24 +5,25 @@ import Title from "../components/Title";
 
 const Page = () => {
   return (
-    <div className="w-full h-[90vh] grid place-items-center">
+    <>
       <Header />
-
       <div className="w-full h-[90vh] grid place-items-center">
-        <Title title="Our Teams" />
-        <div className="w-full py-4 h-[auto] grid place-content-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-4">
-          <Card name="Om salunke" role="Web Developer" />
-          <Card name="Jane Doe" role="App Developer" />
-          <Card name="John Patil" role="Blockchain Developer" />
-          <Card name="Sagar" role="Blockchain Developer" />
-          <Card name="Sagar" role="Blockchain Developer" />
-          <Card name="Sagar" role="Blockchain Developer" />
-          <Card name="Sagar" role="Blockchain Developer" />
-          <Card name="Sagar" role="Blockchain Developer" />
-          <Card name="Sagar" role="Blockchain Developer" />
+        <div className="w-full h-[90vh] grid place-items-center">
+          <Title title="Our Teams" />
+          <div className="w-full py-4 h-[auto] grid place-content-center sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-4">
+            <Card name="Om salunke" role="Web Developer" />
+            <Card name="Jane Doe" role="App Developer" />
+            <Card name="John Patil" role="Blockchain Developer" />
+            <Card name="Sagar" role="Blockchain Developer" />
+            <Card name="Sagar" role="Blockchain Developer" />
+            <Card name="Sagar" role="Blockchain Developer" />
+            <Card name="Sagar" role="Blockchain Developer" />
+            <Card name="Sagar" role="Blockchain Developer" />
+            <Card name="Sagar" role="Blockchain Developer" />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 

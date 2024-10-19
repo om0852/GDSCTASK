fc6f42b2188dcd203f2663269c08689b70fc2492
om salunke <omsaunke343@gmail.com>
2024-10-19 14:58:54 +0530

    done

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Title from '../components/Title'
+import Header from '../components/Header'
+// import { CardFlee } from 'react-cardflee-anim'
+
+const Page = () => {
+  return (
+    <>
+    <Header/>
+    <div className='w-full h-[90vh] grid flex-col justify-center'>
+        <div>
+        <Title title="Contact Us" />
+        <h1>Under construction</h1>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Page
